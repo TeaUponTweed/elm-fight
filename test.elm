2c1343132ca4e3e482ea0cadaf3b6982b95cb9f5
Michael Mason <TeaUponTweed@gmail.com>
2017-02-19 20:57:59 -0700

    change test

--- test.elm
+++ test.elm
@@ -1,16 +1,32 @@
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Debug exposing (log)
+import Html
 
-(.) : String -> a -> String
-(.) str thing = str ++ " " ++ toString thing
+--(.) : String -> a -> String
+--(.) str thing = str ++ " " ++ toString thing
 --mlog: a -> b -> b
 --mlog thing = log (toString thing)
+--view model =
+--    Html.div []
+--    [ Html.p [] [ text "hello world" ]
+--    , main
+--    ]
 
 main =
     let (w, _) = (10, log "width"  (1/10.0)) in
     svg
-        [ version "1.1", x "0", y "0", viewBox ("0 0" . w . w), width "200", height "200"
+        [
+            version "1.1",
+            x "0",
+            y "0",
+            viewBox
+                ("0 0 " ++
+                toString w ++
+                " " ++
+                toString w),
+            width "200",
+            height "200"
         ]
         (  (pusher 0 0 0.95 "#ffffff")
         ++ (pusher 1 0 0.95 "#ffffff")

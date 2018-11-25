5f8655cc43a78981b89979b4ecfe1b0363bebf25
Michael Mason <TeaUponTweed@gmail.com>
2018-11-25 12:48:18 -0700

    changes

--- src/Main.elm
+++ src/Main.elm
@@ -4,9 +4,6 @@ import Browser.Navigation as Nav
 import Html
 import Html.Attributes as Attributes
 
-import Url
-import Url.Parser as Parser exposing (Parser, (</>), custom, fragment, map, oneOf, s, top, string)
-
 import List
 
 import Board
@@ -16,23 +13,20 @@ import Lobby
 -- MAIN
 
 
+main : Program () Model Msg
 main =
-  Browser.application
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    , onUrlRequest = LinkClicked
-    , onUrlChange = UrlChanged
-    }
-
+    Browser.document
+        { init = init
+        , view = \model -> { title = "Elm • TodoMVC", body = [view model] }
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 -- MODEL
 
 
 type alias Model =
-  { key : Nav.Key
-  , page : Page
+  { page : Page
   }
 
 type Page
@@ -52,30 +46,26 @@ subscriptions model =
 -- VIEW
 
 
-view : Model -> Browser.Document Msg
+view : Model -> Html.Html Msg
 view model =
-  let 
-    links =
-      [ Html.li [] [ Html.a [ Attributes.href "/lobby" ] [ Html.text "To Lobby" ] ]
-      , Html.li [] [ Html.a [ Attributes.href "/game/test" ] [ Html.text "To Test Game" ] ]
-      ]
-  in
     case model.page of
       Lobby lobby ->
-        Browser.Document "Lobby" <| List.append links [ Html.map LobbyMsg ( Lobby.view lobby ) ]
+        Html.map LobbyMsg (Lobby.view lobby)
 
       Board board ->
-        Browser.Document "Board" <| List.append links [ Html.map BoardMsg ( Board.view board ) ]
+         Html.map BoardMsg (Board.view board)
 
 -- INIT
 
 
-init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
-init _ url key =
+init : () -> ( Model, Cmd Msg )
+init _ =
   let
     (lobby, _) = Lobby.init ()
   in
-    stepUrl url (Model key (Lobby lobby))
+    (Model (Lobby lobby)
+    , Cmd.none
+    )
 
 
 
@@ -83,9 +73,8 @@ init _ url key =
 
 
 type Msg
-  = NoOp
-  | LinkClicked Browser.UrlRequest
-  | UrlChanged Url.Url
+  = GoToLobby
+  | GoToBoard String
   | BoardMsg Board.Msg
   | LobbyMsg Lobby.Msg
 
@@ -93,24 +82,10 @@ type Msg
 update : Msg -> Model -> ( Model, Cmd Msg )
 update message model =
   case message of
-    NoOp ->
-      ( model, Cmd.none )
-
-    LinkClicked urlRequest ->
-      case urlRequest of
-        Browser.Internal url ->
-          ( model
-          , Nav.pushUrl model.key (Url.toString url)
-          )
-
-        Browser.External href ->
-          ( model
-          , Nav.load href
-          )
-
-    UrlChanged url ->
-      stepUrl url model
-
+    GoToLobby ->
+      stepLobby model (Lobby.init ())
+    GoToBoard gameID ->
+      stepBoard model (Board.init gameID)
     BoardMsg msg ->
       case model.page of
         Board board -> stepBoard model (Board.update msg board)
@@ -133,25 +108,3 @@ stepLobby model (lobby, cmds) =
   ( { model | page = Lobby lobby }
   , Cmd.map LobbyMsg cmds
   )
-
-
--- ROUTER
-
-type Route
-  = GameID String
-  | UserID String
-
-stepUrl : Url.Url -> Model -> (Model, Cmd Msg)
-stepUrl url model =
-  let 
-    parser =
-      oneOf
-        [ map GameID (s "game" </> string)
-        ]
-  in
-    case Parser.parse parser url of
-      Just (GameID gameID) ->
-        stepBoard model (Board.init gameID)
-
-      _ ->
-        stepLobby model (Lobby.init () )

059de2ece991dea6bbb8655daba85f0fb28f6661
Michael Mason <TeaUponTweed@gmail.com>
2020-09-30 21:18:07 -0600

    more update

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 set -e
-# elm make src/Board.elm --output=elm.js
-elm make src/Main.elm --output=elm.js
+elm make src/PF.elm --output=elm.js
 open index.html
--- src/Router.elm
+++
@@ -1,7 +0,0 @@
-module Router exposing (..)
-
-type Msg
-    = GoToLobby
-    | CreateNewGame
-    | GoToGame String
-    | DoNothing

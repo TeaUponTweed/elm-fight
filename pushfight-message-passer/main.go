44c8c56ac5831af9c846a5a113ab46342560ae45
Michael Mason <TeaUponTweed@gmail.com>
2020-10-03 14:11:37 -0600

    update ws handler

--- pushfight-message-passer/main.go
+++ pushfight-message-passer/main.go
@@ -5,64 +5,80 @@
 package main
 
 import (
-	"flag"
-	"log"
-	"net/http"
-	"fmt"
+    "flag"
+    "log"
+    "net/http"
+    "fmt"
 )
 
 var addr = flag.String("addr", ":8080", "http service address")
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL)
-	// if r.URL.Path != "/" {
-	// 	http.Error(w, "Not found", http.StatusNotFound)
-	// 	return
-	// }
-	// if r.Method != "GET" {
-	// 	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	// 	return
-	// }
-	http.ServeFile(w, r, "./index.html")
+    log.Println(r.URL)
+    if r.URL.Path != "/" {
+        http.Error(w, "Not found", http.StatusNotFound)
+        return
+    }
+    if r.Method != "GET" {
+        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+        return
+    }
+    http.ServeFile(w, r, "./index.html")
 }
 func serveJS(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL)
-	// if r.URL.Path != "/" {
-	// 	http.Error(w, "Not found", http.StatusNotFound)
-	// 	return
-	// }
-	// if r.Method != "GET" {
-	// 	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	// 	return
-	// }
-	http.ServeFile(w, r, "./elm.js")
+    log.Println(r.URL)
+    // if r.URL.Path != "/" {
+    //  http.Error(w, "Not found", http.StatusNotFound)
+    //  return
+    // }
+    // if r.Method != "GET" {
+    //  http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+    //  return
+    // }
+    http.ServeFile(w, r, "./elm.js")
 }
 
 func newGame(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL)
-	if r.URL.Path != "/ng" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
-	}
-	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	fmt.Println("hello world")
+    log.Println(r.URL)
+    if r.URL.Path != "/ng" {
+        http.Error(w, "Not found", http.StatusNotFound)
+        return
+    }
+    if r.Method != "GET" {
+        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+        return
+    }
+    fmt.Println("hello world")
 }
 
 func main() {
-	// flag.Parse()
-	// hub := newHub()
-	// go hub.run()
-	http.HandleFunc("/", serveHome)
-	http.HandleFunc("/elm.js", serveJS)
-	// http.HandleFunc("/ng", newGame)
-	// http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-	// 	serveWs(hub, w, r)
-	// })
-	err := http.ListenAndServe(*addr, nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+    hubs := make(map[string]*Hub)
+
+    flag.Parse()
+    // hub := 
+    // go hub.run()
+    http.HandleFunc("/", serveHome)
+    http.HandleFunc("/elm.js", serveJS)
+    // http.HandleFunc("/ng", newGame)
+    http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+        gameIDs, ok := r.URL.Query()["gameID"]
+        
+        if !ok || len(gameIDs[0]) < 1 {
+            log.Println("Url Param 'gameID' is missing")
+            return
+        }
+        for i := 0; i < len(gameIDs); i++ {
+            hub, exists := hubs[gameIDs[i]]
+            if !exists {
+                hub := newHub()
+                go hub.run()
+                hubs[gameIDs[i]] = hub
+            }
+            serveWs(hub, w, r)
+        }
+    })
+    err := http.ListenAndServe(*addr, nil)
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
 }

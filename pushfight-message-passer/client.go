748a18189809a9c066aa54b5b46653bb086c18e1
Michael Mason <TeaUponTweed@gmail.com>
2020-10-04 14:32:54 -0600

    add license and attribution

--- pushfight-message-passer/client.go
+++ pushfight-message-passer/client.go
@@ -1,6 +1,7 @@
 // Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+// 2020 Code modified by Michael Mason from https://github.com/gorilla/websocket/tree/master/examples/chat
 
 package main
 
